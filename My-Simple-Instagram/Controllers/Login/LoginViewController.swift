79f9c0cb3d8372298aec52f0f27100c35dbf4573
Luigi Aiello <luigi-aiello@live.it>
2017-10-30 19:11:40 +0100

    Added profile view controller

---
+++ My-Simple-Instagram/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewController.swift
+//  My-Simple-Instagram
+//
+//  Created by Luigi Aiello on 30/10/17.
+//  Copyright © 2017 Luigi Aiello. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var loginIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK:- Setup
+    private func setup() {
+        let authURL = String(format: "%@?client_id=%@&redirect_uri=%@&response_type=token&scope=%@&DEBUG=True", arguments: [AppConfig.INSTAGRAM_AUTHURL, AppConfig.INSTAGRAM_CLIENT_ID, AppConfig.INSTAGRAM_REDIRECT_URI, AppConfig.INSTAGRAM_SCOPE])
+        let urlRequest = URLRequest.init(url: URL.init(string: authURL)!)
+        webView.delegate = self
+        webView.loadRequest(urlRequest)
+    }
+    
+    // MARK:- Helpers
+    func checkRequestForCallbackURL(request: URLRequest) -> Bool {
+        let requestURLString = (request.url?.absoluteString)! as String
+        //APILOL.INSTAGRAM_REDIRECT_URI
+        if requestURLString.hasPrefix(AppConfig.INSTAGRAM_REDIRECT_URI) {
+            let range: Range<String.Index> = requestURLString.range(of: "#access_token=")!
+            let newStr = String(requestURLString[range.upperBound...])
+            handleAuth(authToken: newStr)
+            return false;
+        }
+        return true
+    }
+    func handleAuth(authToken: String) {
+        print("Token: \(authToken)")
+        API.UserClass.getMyProfile { (success) in
+            print(success)
+        }
+    }
+}
+
+// MARK:- WebView Delegate
+extension LoginViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView,
+                 shouldStartLoadWith request:URLRequest,
+                 navigationType: UIWebViewNavigationType) -> Bool {
+        return checkRequestForCallbackURL(request: request)
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        loginIndicator.isHidden = false
+        loginIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        loginIndicator.isHidden = true
+        loginIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        webViewDidFinishLoad(webView)
+    }
+}
