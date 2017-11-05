3120d477585e26f7f262e2c29d3dddf04ffd4bcb
Luigi Aiello <luigi-aiello@live.it>
2017-11-05 16:11:38 +0100

    Updated instagram keys

--- My-Simple-Instagram/Configurations/AppConfig.swift
+++ My-Simple-Instagram/Configurations/AppConfig.swift
@@ -14,8 +14,8 @@ class AppConfig {
     static let sharedInstance = AppConfig()
     
     static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
-    static let INSTAGRAM_CLIENT_ID = "1a78804cbcb9422c9ceb12d6c6ed941e"
-    static let INSTAGRAM_CLIENTSERCRET = "18a119e2aaf34879855e5520648bea51"
+    static let INSTAGRAM_CLIENT_ID = "2662b83c8a6d4b1a910c677cc638bc2d"
+    static let INSTAGRAM_CLIENTSERCRET = "dab1cc77151446858ffad168ebd2981b"
     static let INSTAGRAM_REDIRECT_URI = "http://www.luigiaiello.org"
     static let INSTAGRAM_ACCESS_TOKEN = "access_token"
     static let INSTAGRAM_SCOPE = "follower_list+public_content" /* add whatever scope you need https://www.instagram.com/developer/authorization/ */
